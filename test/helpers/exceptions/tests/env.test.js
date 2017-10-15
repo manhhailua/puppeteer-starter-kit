532dbc9734d30c75490a84a47d8bb4da04ec50da
manhpt <manhhailua@gmail.com>
2017-10-15 12:50:23 +0900

    use mocking func

--- test/helpers/exceptions/tests/env.test.js
+++ test/helpers/exceptions/tests/env.test.js
@@ -3,19 +3,19 @@ import env from '../env';
 describe('env helpers', () => {
   describe('checkEnvironmentVariables', () => {
     test('should throw custom error if ROOT_HREF is not defined', () => {
-      function mockingFunction() {
-        env.checkEnvironmentVariables(null);
-      }
+      const mockingThrowingError = jest.fn(() => {
+        env.checkEnvironmentVariables('');
+      });
 
-      expect(mockingFunction).toThrowError(env.ERROR_ROOT_HREF_IS_NOT_DEFINED);
+      expect(mockingThrowingError).toThrowError(env.ERROR_ROOT_HREF_IS_NOT_DEFINED);
     });
 
     test('should not throw any error if ROOT_HREF is defined', () => {
-      function mockingFunction() {
+      const mockingThrowingError = jest.fn(() => {
         env.checkEnvironmentVariables('https://google.com/');
-      }
+      });
 
-      expect(mockingFunction).not.toThrowError();
+      expect(mockingThrowingError).not.toThrowError();
     });
   });
 });
