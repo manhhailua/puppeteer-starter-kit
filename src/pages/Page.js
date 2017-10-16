137b3e34074825ed9198f7ae101c3b0373bb2b15
manhpt <manhhailua@gmail.com>
2017-10-16 10:35:38 +0900

    add .goto and .title to Page

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -6,6 +6,22 @@ class Page {
     this.page = page;
     this.href = url.resolve(process.env.ROOT_HREF, path);
   }
+
+  async goto(options) {
+    await this.page.goto(this.href, options);
+  }
+
+  async title() {
+    let title = null;
+
+    try {
+      title = await this.page.title();
+    } catch (error) {
+      return error;
+    }
+
+    return title;
+  }
 }
 
 export default Page;
